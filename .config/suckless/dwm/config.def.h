63cb46b5a759915db3b8da59286b35a9137d33f6
hooxoo <ecstaiji@gmail.com>
2022-04-02 21:22:39 +0900

    edit bar colors and gaps

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -5,14 +5,14 @@ static const unsigned int borderpx = 5; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int swallowfloating =
     0; /* 1 means swallow floating windows by default */
-static const unsigned int gappih = 10; /* horiz inner gap between windows */
-static const unsigned int gappiv = 10; /* vert inner gap between windows */
+static const unsigned int gappih = 5; /* horiz inner gap between windows */
+static const unsigned int gappiv = 5; /* vert inner gap between windows */
 static const unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
+    5; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
     10; /* vert outer gap between windows and screen edge */
 static const int smartgaps_fact =
-    1; /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer
+    5; /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer
           gaps */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
@@ -249,7 +249,8 @@ static Key keys[] = {
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_n, setlayout, {0}},
+    {MODKEY, XK_n, setlayout, {.v = &layouts[3]}},
+    {MODKEY, XK_period, setlayout, {0}},
     {MODKEY, XK_space, togglefloating, {0}},
     {MODKEY | ShiftMask, XK_f, togglefullscreen, {0}},
     /* {MODKEY, XK_0, view, {.ui = ~0}}, */
