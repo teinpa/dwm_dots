d4d822012d8ffa820d4a9b7e0ec56d438e5cbb2f
hooxoo <ecstaiji@gmail.com>
2022-04-09 23:00:52 +0900

    remove unused

--- .config/slimzsh/slim.zsh
+++ .config/slimzsh/slim.zsh
@@ -15,7 +15,6 @@ setopt NO_NOMATCH
 
 export CLICOLOR=1
 export LESSHISTFILE=-
-export FZF_DEFAULT_OPTS='--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD'
 
 source $slim_path/keys.zsh
 source $slim_path/history.zsh
