e495210f9af008f4bddc5445a4aa2c3a14835cb1
hooxoo <ecstaiji@gmail.com>
2022-04-11 21:51:53 +0900

    reuse slimzsh

--- .config/slimzsh/slim.zsh
+++ .config/slimzsh/slim.zsh
@@ -3,12 +3,23 @@ fpath=( $slim_path $fpath )
 
 # export PATH=~/.local/bin:$PATH
 
+HISTFILE=$slim_path/.HISTFILE
+HISTSIZE=1000
+SAVEHIST=1000
+unsetopt beep
+bindkey -v
+
 # autoload -U promptinit && promptinit
 # prompt pure
 export STARSHIP_CONFIG=~/.config/starship/pastel-powerline.toml
 
-autoload -U compinit
-compinit
+autoload -Uz compinit
+
+for dump in ~/.zcompdump(N.mh+24); do
+   compinit
+done
+
+compinit -C
 
 setopt autocd
 setopt extendedglob
@@ -26,8 +37,9 @@ source $slim_path/correction.zsh
 source $slim_path/stack.zsh
 source $slim_path/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 
-if command -v fasd >/dev/null 2>&1; then
-  eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
-fi
+# if command -v fasd >/dev/null 2>&1; then
+#   eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
+# fi
 
 eval "$(starship init zsh)"
+
--- .zshrc
+++ .zshrc
@@ -1,10 +1,5 @@
-HISTFILE=~/.config/slimzsh/.HISTFILE
-HISTSIZE=1000
-SAVEHIST=1000
-unsetopt beep
-bindkey -v
-
-autoload -Uz compinit
-compinit
+# zmodload zsh/zprof
 
 source "$HOME/.config/slimzsh/slim.zsh"
+
+# zprof
