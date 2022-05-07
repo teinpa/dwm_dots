9591e6724273b603d7867ac84f43b18f59c9e3a4
hooxoo <ecstaiji@gmail.com>
2022-05-07 21:45:46 +0900

    cleanup code

--- .config/scripts/statusbar.sh
+++ .config/scripts/statusbar.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# ^c$var^ = fg color
-# ^b$var^ = bg color
-
 interval=0
 # load colors
 . ~/.config/scripts/cat_stat
@@ -60,16 +57,6 @@ get_volume(){
         fi
 }
 
-# battery() {
-# 	get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
-# 	printf "^c$blue^   $get_capacity"
-# }
-
-# brightness() {
-# 	printf "^c$red^   "
-# 	printf "^c$red^%.0f\n" $(cat /sys/class/backlight/*/brightness)
-# }
-
 mem() {
   memory=$(free -h | awk '/^Mem/ { print $3 }')
   
@@ -83,15 +70,7 @@ weather() {
 
   printf "^c$lavender^ $cond ^c$red^$temp"
   # printf "^c$red^ $temp"
-  #dunstify "$(curl -s 'wttr.in/Seoul?format=%C+%t' | sed 's/+//g')"
 }
-
-# wlan() {
-# 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-# 	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
-# 	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
-# 	esac
-# }
 
 clock() {
   printf "^c$lavender^ $(date '+%a ' | sed 's/.*/\U&/')"
