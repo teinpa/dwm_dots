2385db87597cf6a413a4bdb2dc78c9edd720e855
hooxoo <ecstaiji@gmail.com>
2022-05-07 21:48:30 +0900

    cleanup code

--- .config/slimzsh/slim.zsh
+++ .config/slimzsh/slim.zsh
@@ -14,11 +14,6 @@ bindkey -v
 export STARSHIP_CONFIG=~/.config/starship/tags.toml
 
 autoload -Uz compinit
-
-# for dump in ~/.zcompdump(N.mh+24); do
-#    compinit
-# done
-
 compinit -C
 
 setopt autocd
@@ -37,11 +32,6 @@ source $slim_path/correction.zsh
 source $slim_path/sudo.plugin.zsh
 source $slim_path/stack.zsh
 source $slim_path/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-
-# export FZF_DEFAULT_OPTS='--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD'
-# if command -v fasd >/dev/null 2>&1; then
-#   eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
-# fi
 
 eval "$(starship init zsh)"
 
