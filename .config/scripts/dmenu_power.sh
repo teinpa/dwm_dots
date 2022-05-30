1a49d8ecfd43a3229b5492d49774fdbefabfd3b5
hooxoo <ecstaiji@gmail.com>
2022-05-30 22:24:04 +0900

    adjust horizontal padding

--- .config/scripts/dmenu_power.sh
+++ .config/scripts/dmenu_power.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RET=$(echo -e "shutdown\nreboot\nlogout\nlock\ncancel" | dmenu -nb "#1e1e2e" -nf "#D9E0EE" -sb "#f28fad" -sf "#1e1e2e" -Y 7 -X 10 -W 700 -p "POWER:")
+RET=$(echo -e "shutdown\nreboot\nlogout\nlock\ncancel" | dmenu -nb "#1e1e2e" -nf "#D9E0EE" -sb "#f28fad" -sf "#1e1e2e" -Y 7 -X 15 -W 700 -p "POWER:")
 
 case $RET in
 	shutdown) poweroff ;;
