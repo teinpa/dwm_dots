15a816b6e00d1d29c22d963e81bff8bd3828b48e
hooxoo <ecstaiji@gmail.com>
2022-06-13 16:54:03 +0900

    update colors

--- .config/scripts/dmenu_power.sh
+++ .config/scripts/dmenu_power.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RET=$(echo -e "shutdown\nreboot\nlogout\nlock\ncancel" | dmenu -nb "#1e1e2e" -nf "#D9E0EE" -sb "#f28fad" -sf "#1e1e2e" -Y 7 -X 15 -W 700 -p "POWER:")
+RET=$(echo -e "shutdown\nreboot\nlogout\nlock\ncancel" | dmenu -nb "#1e1e2e" -nf "#c6d0f5" -sb "#f38ba8" -sf "#1e1e2e" -Y 7 -X 15 -W 700 -p "POWER:")
 
 case $RET in
 	shutdown) poweroff ;;
--- .config/scripts/dmenu_screenshot.sh
+++ .config/scripts/dmenu_screenshot.sh
@@ -29,7 +29,7 @@ shotarea () {
   notify_user
 }
 
-SHOT=$(echo -e "fullscreen\nwindow\narea\ncancel" | dmenu -nb "#1e1e2e" -nf "#D9E0EE" -sb "#96cdfb" -sf "#1e1e2e" -Y 7 -X 15 -W 700 -p "CAPTURE:")
+SHOT=$(echo -e "fullscreen\nwindow\narea\ncancel" | dmenu -nb "#1e1e2e" -nf "#c6d0f5" -sb "#96cdfb" -sf "#1e1e2e" -Y 7 -X 15 -W 700 -p "CAPTURE:")
 
 case $SHOT in
   fullscreen) shotnow ;;
