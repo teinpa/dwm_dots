742312e81c638f30561c742d573a79b1b63b0fec
hooxoo <ecstaiji@gmail.com>
2022-06-15 15:58:31 +0900

    change workspace names

--- .config/suckless/dwm-flexipatch/config.def.h
+++ .config/suckless/dwm-flexipatch/config.def.h
@@ -5,12 +5,12 @@ static const unsigned int borderpx = 5; /* border pixel of windows */
 static const unsigned int snap = 5;     /* snap pixel */
 static const int swallowfloating =
     0; /* 1 means swallow floating windows by default */
-static const unsigned int gappih = 10; /* horiz inner gap between windows */
-static const unsigned int gappiv = 10; /* vert inner gap between windows */
+static const unsigned int gappih = 20; /* horiz inner gap between windows */
+static const unsigned int gappiv = 20; /* vert inner gap between windows */
 static const unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
+    70; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    10; /* vert outer gap between windows and screen edge */
+    100; /* vert outer gap between windows and screen edge */
 static const int smartgaps_fact =
     1; /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer
           gaps */
