d956de046746f444801aa1ed8503f6d324d4aabf
hooxoo <ecstaiji@gmail.com>
2022-06-18 13:09:19 +0900

    adjust size

--- .config/scripts/dmenu_music.sh
+++ .config/scripts/dmenu_music.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RET=$(echo -e "play-pause\nnext\nprevious\nstop\ncancel" | dmenu -nb "#1e1e2e" -nf "#c6d0f5" -sb "#a6e3a1" -sf "#1e1e2e" -Y 7 -X 15 -W 700 -p "MUSIC:")
+RET=$(echo -e "play-pause\nnext\nprevious\nstop\ncancel" | dmenu -nb "#1e1e2e" -nf "#c6d0f5" -sb "#a6e3a1" -sf "#1e1e2e" -Y 7 -X 20 -W 2520 -p "MUSIC:")
 
 case $RET in
   play-pause) playerctl play-pause ;;
